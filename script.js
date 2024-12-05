ca9fb4ddb12af339f65aab689f3f5f2ca9160b07
Jules <bertron.simpson@gmail.com>
2024-12-05 16:12:23 +0900

    new main page

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Toggle the central logo image on click
+document.getElementById('logo-image').addEventListener('click', function() {
+  const logo = this;
+  if (logo.src.includes('logo.png')) {
+    logo.src = 'assets/logo2.png';
+  } else {
+    logo.src = 'assets/logo.png';
+  }
+});
